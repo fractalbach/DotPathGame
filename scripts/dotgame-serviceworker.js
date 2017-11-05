4970226d5a58d7bd9a2d1c5e016985b98ad6022f
fractalbach <4bach@protonmail.com>
2017-11-04 23:10:34 -0700

    added a service worker close #11
    Should cache the page when installed and delete any service workers.
    Then when loading a page, it should use that cache.
    
    Still Needs to be checked to see if it works correctly.

---
+++ scripts/dotgame-serviceworker.js
@@ -0,0 +1,96 @@
+if ('serviceWorker' in navigator) {
+  window.addEventListener('load', function() {
+    navigator.serviceWorker.register('/sw.js').then(function(registration) {
+      // Registration was successful
+      console.log('ServiceWorker registration successful with scope: ', registration.scope);
+    }, function(err) {
+      // registration failed :(
+      console.log('ServiceWorker registration failed: ', err);
+    });
+  });
+}
+
+// Globally, Define names for all of the caches that will be used.  
+// Used for both creation and deletion of the caches.
+var CACHE_LIST = ['dot-game-cache-v1'];
+
+// Create a cache of the main pages for the game.  
+var CACHE_NAME = CACHE_LIST[0];
+
+
+// Things to run when the service worker is first installed.
+// Creates a cache of the specified URLs
+self.addEventListener('install', function(event) {
+    var urlsToCache = [
+      '/'
+    ];
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(function(cache) {
+        console.log('Opened cache');
+        return cache.addAll(urlsToCache);
+      })
+  );
+});
+
+
+
+// Loop through caches that aren't on the whitelisted and delete them.
+// Uses the names from the global cache list variable.
+self.addEventListener('activate', function(event) {
+  var cacheWhitelist = CACHE_LIST;
+  event.waitUntil(
+    caches.keys().then(function(cacheNames) {
+      return Promise.all(
+        cacheNames.map(function(cacheName) {
+          if (cacheWhitelist.indexOf(cacheName) === -1) {
+            return caches.delete(cacheName);
+          }
+        })
+      );
+    })
+  );
+});
+
+
+
+
+self.addEventListener('fetch', function(event) {
+  event.respondWith(
+    caches.match(event.request)
+      .then(function(response) {
+        // Cache hit - return response
+        if (response) {
+          return response;
+        }
+
+        // IMPORTANT: Clone the request. A request is a stream and
+        // can only be consumed once. Since we are consuming this
+        // once by cache and once by the browser for fetch, we need
+        // to clone the response.
+        var fetchRequest = event.request.clone();
+
+        return fetch(fetchRequest).then(
+          function(response) {
+            // Check if we received a valid response
+            if(!response || response.status !== 200 || response.type !== 'basic') {
+              return response;
+            }
+
+            // IMPORTANT: Clone the response. A response is a stream
+            // and because we want the browser to consume the response
+            // as well as the cache consuming the response, we need
+            // to clone it so we have two streams.
+            var responseToCache = response.clone();
+
+            caches.open(CACHE_NAME)
+              .then(function(cache) {
+                cache.put(event.request, responseToCache);
+              });
+
+            return response;
+          }
+        );
+      })
+    );
+});
