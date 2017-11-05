fd4eb55ed76c882eb27d89acbf9f8940a4e4fd4e
fractalbach <4bach@protonmail.com>
2017-11-05 01:47:13 -0800

    minor performance changes
    wrapped the service worker code with
    Event Listener for "DOMContentLoaded"
    So that it doesn't start loading too early.

--- sw.js
+++ sw.js
@@ -1,93 +1,105 @@
-if ('serviceWorker' in navigator) {
-  window.addEventListener('load', function() {
-    navigator.serviceWorker.register('sw.js').then(function(registration) {
-      // Registration was successful
-      console.log('ServiceWorker registration successful with scope: ', registration.scope);
-    }, function(err) {
-      // registration failed :(
-      console.log('ServiceWorker registration failed: ', err);
+// Service Workers
+
+
+// Encapsulate the entire service worker script and wait for the parser.
+document.addEventListener("DOMContentLoaded", function(event) {
+  console.log("DOM fully loaded and parsed");
+
+
+  if ('serviceWorker' in navigator) {
+    window.addEventListener('load', function() {
+      navigator.serviceWorker.register('sw.js').then(function(registration) {
+        // Registration was successful
+        console.log('ServiceWorker registration successful with scope: ', registration.scope);
+      }, function(err) {
+        // registration failed :(
+        console.log('ServiceWorker registration failed: ', err);
+      });
     });
-  });
-}
+  }
 
-// Create a cache of the main pages for the game.  
-// Things to run when the service worker is first installed.
-// Creates a cache of the specified URLs
-self.addEventListener('install', function(event) {
-  var CACHE_NAME = 'dot-game-cache-v1';
-    var urlsToCache = [
-      'https://fractalbach.github.io/DotPathGame/index.html'
-    ];
-  event.waitUntil(
-    caches.open(CACHE_NAME)
-      .then(function(cache) {
-        console.log('Opened cache');
-        return cache.addAll(urlsToCache);
-      })
-  );
-});
-
-
-// Enables Updates for service workers.
-// Loop through caches that aren't on the whitelisted and delete them.
-// Uses the names from the global cache list variable.
-self.addEventListener('activate', function(event) {
-  var cacheWhitelist = ['dot-game-cache-v1'];
-  event.waitUntil(
-    caches.keys().then(function(cacheNames) {
-      return Promise.all(
-        cacheNames.map(function(cacheName) {
-          if (cacheWhitelist.indexOf(cacheName) === -1) {
-            return caches.delete(cacheName);
-          }
+  // Create a cache of the main pages for the game.  
+  // Things to run when the service worker is first installed.
+  // Creates a cache of the specified URLs
+  self.addEventListener('install', function(event) {
+    var CACHE_NAME = 'dot-game-cache-v1';
+      var urlsToCache = [
+        'https://fractalbach.github.io/DotPathGame/index.html'
+      ];
+    event.waitUntil(
+      caches.open(CACHE_NAME)
+        .then(function(cache) {
+          console.log('Opened cache');
+          return cache.addAll(urlsToCache);
         })
-      );
-    })
-  );
-});
+    );
+  });
 
 
-
-
-self.addEventListener('fetch', function(event) {
-
-  var CACHE_NAME = 'dot-game-cache-v1';
-
-  event.respondWith(
-    caches.match(event.request)
-      .then(function(response) {
-        // Cache hit - return response
-        if (response) {
-          return response;
-        }
-
-        // IMPORTANT: Clone the request. A request is a stream and
-        // can only be consumed once. Since we are consuming this
-        // once by cache and once by the browser for fetch, we need
-        // to clone the response.
-        var fetchRequest = event.request.clone();
-
-        return fetch(fetchRequest).then(
-          function(response) {
-            // Check if we received a valid response
-            if(!response || response.status !== 200 || response.type !== 'basic') {
-              return response;
+  // Enables Updates for service workers.
+  // Loop through caches that aren't on the whitelisted and delete them.
+  // Uses the names from the global cache list variable.
+  self.addEventListener('activate', function(event) {
+    var cacheWhitelist = ['dot-game-cache-v1'];
+    event.waitUntil(
+      caches.keys().then(function(cacheNames) {
+        return Promise.all(
+          cacheNames.map(function(cacheName) {
+            if (cacheWhitelist.indexOf(cacheName) === -1) {
+              return caches.delete(cacheName);
             }
-
-            // IMPORTANT: Clone the response. A response is a stream
-            // and because we want the browser to consume the response
-            // as well as the cache consuming the response, we need
-            // to clone it so we have two streams.
-            var responseToCache = response.clone();
-
-            caches.open(CACHE_NAME)
-              .then(function(cache) {
-                cache.put(event.request, responseToCache);
-              });
-
-            return response;
-          }
+          })
         );
       })
     );
+  });
+
+
+
+
+  self.addEventListener('fetch', function(event) {
+
+    var CACHE_NAME = 'dot-game-cache-v1';
+
+    event.respondWith(
+      caches.match(event.request)
+        .then(function(response) {
+          // Cache hit - return response
+          if (response) {
+            return response;
+          }
+
+          // IMPORTANT: Clone the request. A request is a stream and
+          // can only be consumed once. Since we are consuming this
+          // once by cache and once by the browser for fetch, we need
+          // to clone the response.
+          var fetchRequest = event.request.clone();
+
+          return fetch(fetchRequest).then(
+            function(response) {
+              // Check if we received a valid response
+              if(!response || response.status !== 200 || response.type !== 'basic') {
+                return response;
+              }
+
+              // IMPORTANT: Clone the response. A response is a stream
+              // and because we want the browser to consume the response
+              // as well as the cache consuming the response, we need
+              // to clone it so we have two streams.
+              var responseToCache = response.clone();
+
+              caches.open(CACHE_NAME)
+                .then(function(cache) {
+                  cache.put(event.request, responseToCache);
+                });
+
+              return response;
+            }
+          );
+        })
+      );
+  });
+
+
+//end of encapsulation (wait for dom parse)
 });
